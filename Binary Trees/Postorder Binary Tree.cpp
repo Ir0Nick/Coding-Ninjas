d5f9864d08b5d3368dd181ba5022e48f77b50d48
Ir0Nick <62323588+Ir0Nick@users.noreply.github.com>
2020-06-12 22:36:49 +0530

    Create Postorder Binary Tree.cpp

---
+++ Binary Trees/Postorder Binary Tree.cpp
@@ -0,0 +1,26 @@
+// Following is the Binary Tree node structure
+/**************
+ class BinaryTreeNode {
+ public :
+ T data;
+ BinaryTreeNode<T> *left;
+ BinaryTreeNode<T> *right;
+ 
+ BinaryTreeNode(T data) {
+ this -> data = data;
+ left = NULL;
+ right = NULL;
+ }
+ };
+ ***************/
+
+void postOrder(BinaryTreeNode<int> *root) {
+  if(root)
+  {
+      postOrder(root->left);
+      postOrder(root->right);
+      cout<<root->data<<' ';
+      
+  }
+    
+}
